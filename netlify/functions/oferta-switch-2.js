4f0a07ad6e165989e0f58da06e177196c46066c7
davidbauer92 <david.bauer_@hotmail.com>
2026-05-19 15:00:16 -0300

    Update oferta-switch-2.js

--- netlify/functions/oferta-switch-2.js
+++ netlify/functions/oferta-switch-2.js
@@ -1,26 +1,26 @@
 exports.handler = async function () {
-const produtos = [
-  {
-    nome: "Nintendo Switch 2",
-    loja: "Amazon",
-    categoria: "Console",
-    precoAtual: "R$ 4.199,00",
-    precoMedio: "Preço médio: R$ 4.499,00",
-    statusPreco: "Preço conferido manualmente. Pode mudar a qualquer momento.",
-    imagem: "https://m.media-amazon.com/images/I/714-Fh3ngmL._AC_SX679_.jpg",
-    link: "https://www.amazon.com.br/dp/B0F3GWXLTS?tag=dcbauer0d-20"
-  },
-  {
-    nome: "Nintendo Switch 2 Pro Controller Resident Evil Requiem Edition",
-    loja: "Amazon",
-    categoria: "Acessório",
-    precoAtual: "R$ 661,85",
-    precoMedio: "Preço médio: R$ 749,90",
-    statusPreco: "Preço conferido manualmente. Pode mudar a qualquer momento.",
-    imagem: "https://m.media-amazon.com/images/I/71eeLrGBskL._AC_SX679_.jpg",
-    link: "https://www.amazon.com.br/dp/B0GGJB5FDJ?tag=dcbauer0d-20"
-  }
-];
+  const produtos = [
+    {
+      nome: "Nintendo Switch 2",
+      loja: "Amazon",
+      categoria: "Console",
+      precoAtual: "R$ 4.199,00",
+      precoMedio: "Preço médio: R$ 4.499,00",
+      statusPreco: "Preço conferido manualmente. Pode mudar a qualquer momento.",
+      imagem: "https://m.media-amazon.com/images/I/714-Fh3ngmL._AC_SX679_.jpg",
+      link: "https://www.amazon.com.br/dp/B0F3GWXLTS?tag=dcbauer0d-20"
+    },
+    {
+      nome: "Nintendo Switch 2 Pro Controller Resident Evil Requiem Edition",
+      loja: "Amazon",
+      categoria: "Acessório",
+      precoAtual: "R$ 661,85",
+      precoMedio: "Preço médio: R$ 749,90",
+      statusPreco: "Preço conferido manualmente. Pode mudar a qualquer momento.",
+      imagem: "https://m.media-amazon.com/images/I/71eeLrGBskL._AC_SX679_.jpg",
+      link: "https://www.amazon.com.br/dp/B0GGJB5FDJ?tag=dcbauer0d-20"
+    }
+  ];
 
   return {
     statusCode: 200,
