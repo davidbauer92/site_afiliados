eb9e0e350fdc233c9891d619ba89fa4040ca9445
davidbauer92 <david.bauer_@hotmail.com>
2026-05-19 14:32:35 -0300

    Add files via upload

--- netlify/functions/oferta-switch-2.js
+++ netlify/functions/oferta-switch-2.js
@@ -1,19 +1,22 @@
 exports.handler = async function () {
-  const oferta = {
-    nome: "Nintendo Switch 2",
-    loja: "Amazon",
-    precoAtual: "R$ 4.199,00",
-    precoMedio: "Preço médio: R$ 4.499,00",
-    statusPreco: "Preço conferido manualmente. Pode mudar a qualquer momento.",
-    imagem: "https://via.placeholder.com/500x350?text=Nintendo+Switch+2",
-    link: "https://www.amazon.com.br/dp/B0F3GWXLTS?tag=dcbauer0d-20"
-  };
+  const produtos = [
+    {
+      nome: "Nintendo Switch 2",
+      loja: "Amazon",
+      categoria: "Console",
+      precoAtual: "R$ 4.199,00",
+      precoMedio: "Preço médio: R$ 4.499,00",
+      statusPreco: "Preço conferido manualmente. Pode mudar a qualquer momento.",
+      imagem: "https://m.media-amazon.com/images/I/714-Fh3ngmL._AC_SX679_.jpg",
+      link: "https://www.amazon.com.br/dp/B0F3GWXLTS?tag=dcbauer0d-20"
+    }
+  ];
 
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(oferta)
+    body: JSON.stringify(produtos)
   };
 };
